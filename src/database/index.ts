fbb89282599d851943ef9208295cf52e86cfe2e6
Igor Puchkov <igor@puchkov.com>
2024-04-07 21:04:53 +0200

    feat(database): add deleteTaskFromDatabase function
    Add a new function deleteTaskFromDatabase to delete a task from the database using its ID.

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 interface DatabaseConfig {
     databaseName: string;
@@ -18,6 +18,28 @@ export async function insertTaskIntoDatabase(taskData: { title: string, descript
 
         // Вставляем данные задачи в коллекцию tasks
         await tasksCollection.insertOne(taskData);
+    } finally {
+        // Всегда закрываем соединение с базой данных после завершения операции
+        await client.close();
+    }
+}
+
+// Функция для удаления задачи из базы данных
+export async function deleteTaskFromDatabase(taskId: string, config: DatabaseConfig): Promise<void> {
+    const { databaseName, uri } = config;
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+
+        const database = client.db(databaseName);
+        const tasksCollection = database.collection('tasks');
+
+        // Создаем объект ObjectId из строки taskId
+        const objectId = new ObjectId(taskId);
+
+        // Удаляем задачу из коллекции по её идентификатору
+        await tasksCollection.deleteOne({ _id: objectId });
     } finally {
         // Всегда закрываем соединение с базой данных после завершения операции
         await client.close();
