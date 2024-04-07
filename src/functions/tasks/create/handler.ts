7783b9ef03974369f86a6f5bd7fddb5b2734d231
Igor Puchkov <igor@puchkov.com>
2024-04-07 21:05:46 +0200

    Clean up

--- src/functions/tasks/create/handler.ts
+++ src/functions/tasks/create/handler.ts
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { formatJSONResponse } from '../../../libs/api-gateway';
 import {insertTaskIntoDatabase} from '../../../database';
