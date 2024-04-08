0a60b295b0597acf186cdbc4ed6cee27d1cad3cf
Igor Puchkov <igor@puchkov.com>
2024-04-08 21:05:21 +0200

    test: remove 'src/database' from Jest test roots
    This commit updates the Jest configuration by removing 'src/database' from the test roots, focusing only on 'src/functions' directory.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    roots: ['<rootDir>/src/functions', '<rootDir>/src/database'],
+    roots: ['<rootDir>/src/functions'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
