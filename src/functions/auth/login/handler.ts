fa6eea11cc47a93dadc1a33e3f77caac93bb6f3d
Igor Puchkov <igor@puchkov.com>
2024-04-11 16:46:55 +0200

    feat(auth): add authentication handling
    Adds authentication handling to the login user handler

--- src/functions/auth/login/handler.ts
+++ src/functions/auth/login/handler.ts
@@ -1,23 +1,25 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
 import {loginUser} from '../../../infrastructure/cognito';
 import {formatJSONResponse} from '../../../libs/api-gateway';
-import {handleError} from '../../../helpers';
+import {handleAuthentication, handleError} from '../../../helpers';
 
 export const loginHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        // Получаем параметры из запроса
-        const requestBody = JSON.parse(event.body);
-        const {email, password} = requestBody;
+    return handleAuthentication(event, async (requestBody) => {
+            try {
+                // Получаем параметры из запроса
+                const {email, password} = requestBody;
 
-        // Пытаемся инициировать аутентификацию через Cognito
-        const result = await loginUser(email, password);
+                // Пытаемся инициировать аутентификацию через Cognito
+                const result = await loginUser(email, password);
 
-        // Возвращаем успешный ответ
-        return formatJSONResponse(result, 200);
-    } catch (error) {
-        // Обрабатываем ошибку подтверждения
-        return handleError(error);
-    }
+                // Возвращаем успешный ответ
+                return formatJSONResponse(result, 200);
+            } catch (error) {
+                // Обрабатываем ошибку подтверждения
+                return handleError(error);
+            }
+        }
+    )
 };
 
 export const main = loginHandler;
